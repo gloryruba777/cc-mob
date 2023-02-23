d33a3f339ebad51855f7a04a4ed529143a4bc502
gloryruba777 <84583697+gloryruba777@users.noreply.github.com>
2023-02-23 13:50:33 +0530

    ajax func

--- cancer.php
+++ cancer.php
@@ -37,7 +37,6 @@
       <div class="iphone-13-pro-max-7-child4"></div>
       <div class="iphone-13-pro-max-7-child5"></div>
       <div class="iphone-13-pro-max-7-child6"></div>
-      <div class="iphone-13-pro-max-7-child7"></div>
       <div class="battery-icon-parent3">
         <img class="battery-icon5" alt="" src="public/batteryicon.svg" />
         <div class="div5">58 %</div>
@@ -53,6 +52,7 @@
         <div class="gs5">GS</div>
         <img class="signal-icon5" alt="" src="public/signal4.svg" />
       </div>
+      <form action="" method="POST">
       <select class="ctype">
       <option class="ctype" id="">Cancer Type</option>
       <?php   
@@ -62,30 +62,29 @@
       <option class="ctype" value=""><?php echo $value['cancerName']; ?></option>
       <?php } ?>
        </select>
-      <select class="tmtype">
-      <option class="tmtype" id="">TM Type</option>
+      <select class="tmtype" id="Tmtype" name="Tmtype">
+      <option class="tmtype">TM Type</option>
       <?php   
         $query = "SELECT DISTINCT(Tmtype) FROM cancerdetails";
         $exequery = mysqli_query($conn, $query);
         while($value = mysqli_fetch_assoc($exequery)) { ?>
-      <option class="tmtype" value=""><?php echo $value['Tmtype']; ?></option>
+      <option class="tmtype" value="<?php echo $value['Tmtype']; ?>"><?php echo $value['Tmtype']; ?></option>
       <?php } ?>
        </select>
-      <select class="ntype">
+      <select class="ntype"  id="Ntype" name="Ntype" onchange="changeType()">
       <option class="ntype" id="">N Type</option>
       <?php   
         $query = "SELECT DISTINCT(Ntype) FROM cancerdetails";
         $exequery = mysqli_query($conn, $query);
         while($value = mysqli_fetch_assoc($exequery)) { ?>
-      <option class="ntype" value=""><?php echo $value['Ntype']; ?></option>
+      <option class="ntype" value="<?php echo $value['Ntype']; ?>"><?php echo $value['Ntype']; ?></option>
       <?php } ?>
        </select>
-      <div class="cstage">Cancer Stage </div>
-      <div class="survyear">Survival Year Calc</div>
-      <div class="survdata">( Last 5 or 10 Yrs data)</div>
-      <div class="survrate">Survival Rate</div>
-      <div class="linksrc">Link Source</div>
-      <div class="treating-doctors">Treating Doctors</div>
+      <div class="cstage" id="cancerStage" name="cancerStage">Cancer Stage</div>
+      <div class="survyear" id="survivalYear" name="survivalYear">Survival Year Calc</div>
+      <div class="survdata" >( Last 5 or 10 Yrs data)</div>
+      <div class="survrate" id="survivalRate" name="survivalRate" >Survival Rate</div>
+      <div class="linksrc" id="link" name="link" >Link Source</div>
       <div class="rectangle-parent7">
         <div class="group-child8"></div>
         <img class="shape-icon8" alt="" src="public/shape.svg" />
@@ -101,5 +100,38 @@
         src="public/logo600x6002removebgpreview-2@2x.png"
       />
     </div>
+        </form>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+    <script>
+      function changeType() {
+          var Tm_type = document.getElementById("Tmtype").value;
+          var N_type  = document.getElementById("Ntype").value;
+          alert(Tm_type + "hi");
+          alert(N_type + "hi");
+            $.ajax({
+            url: "ajaxLoad.php",
+            type: "POST",
+            data: {
+            Tm_type: Tm_type,
+            N_type : N_type
+            },
+
+            cache: false,
+            success: function(result){
+              var rVal= result.split('|');
+              var stage = rVal[0];
+              var year = rVal[1];
+              var rate = rVal[2];
+              var link = rVal[3];
+             $("#cancerStage").html(stage);
+             $("#survivalYear").html(year);
+             $("#survivalRate").html(rate);
+             $("#link").html(link);
+
+            }
+            }); 
+            }
+    </script>
   </body>
 </html>
