1e32723237602059623244686a71479ae71e4ec1
gloryruba777 <84583697+gloryruba777@users.noreply.github.com>
2023-02-23 18:38:27 +0530

    update ajaxLoad

--- ajaxLoad.php
+++ ajaxLoad.php
@@ -3,13 +3,7 @@ include "config.php";
 $tmtype = $_POST["Tm_type"];
 $ntype = $_POST["N_type"];
 $result = mysqli_query($conn,"SELECT * FROM cancerdetails WHERE Tmtype='$tmtype' AND Ntype='$ntype'");
-
 ?>
-<?php
-while($row = mysqli_fetch_array($result)) {
-    
-?>
+<?php while($row = mysqli_fetch_array($result)) { ?>
 <?php echo $row["cancerStage"] ."|". $row["survivalYear"] ."|". $row["survivalRate"] ."|". $row["link"]; ?>
-<?php
-}
-?> 
+<?php } ?> 
